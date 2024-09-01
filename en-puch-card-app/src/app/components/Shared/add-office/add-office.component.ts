6dc2e2bb04c2dcc65449bd5407e3b7e4145946ba
Amirhossein Alisedaghat <amirhosseinalisedaghat@gmail.com>
2024-09-01 10:30:45 +0330

    change the add office form title

--- en-puch-card-app/src/app/components/Shared/add-office/add-office.component.ts
+++ en-puch-card-app/src/app/components/Shared/add-office/add-office.component.ts
@@ -8,19 +8,23 @@ import { Component } from '@angular/core';
 export class AddOfficeComponent {
   currentStep: number = 1;
   progressBar:number = 0;
+  formHeader:string = 'Add Office';
   nextStep(stepNumber: number) {
     switch (stepNumber) {
       case 1:
         this.currentStep = 2;
         this.progressBar = 25;
+        this.formHeader = 'Choose scope of your business';
         break;
       case 2:
         this.currentStep = 3;
         this.progressBar = 50;
+        this.formHeader = 'Office Nickname';
         break;
       case 3:
         this.currentStep = 4;
         this.progressBar = 75;
+        this.formHeader = 'Number of days you want to use';
         break;
       case 4:
         this.progressBar = 100;
@@ -34,14 +38,17 @@ export class AddOfficeComponent {
       case 4:
         this.currentStep = 3;
         this.progressBar = 50;
+        this.formHeader = 'Office Nickname';
         break;
       case 3:
         this.currentStep = 2;
         this.progressBar = 25;
+        this.formHeader = 'Choose scope of your business';
         break;
       case 2:
         this.currentStep = 1;
         this.progressBar = 0;
+        this.formHeader = 'Add Office';
         break;
       case 1:
         this.progressBar = 0;
