e67f2f2e5e62e09e367eb1e1c0404e3b6517f8dd
Amirhossein Alisedaghat <amirhosseinalisedaghat@gmail.com>
2024-10-13 12:04:37 +0330

    set logic of reports

--- en-puch-card-app/src/app/components/Dashboard/Reports/report/report.component.ts
+++ en-puch-card-app/src/app/components/Dashboard/Reports/report/report.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-report',
   templateUrl: './report.component.html',
-  styleUrl: './report.component.css'
+  styleUrl: './report.component.css',
 })
 export class ReportComponent {
-
+  numberOfReport: number | undefined;
+  showReport:boolean | undefined = false;
+  switchBtns(numberSwitch: number) {
+    this.numberOfReport = numberSwitch;
+    this.showReport = true;
+  }
 }
