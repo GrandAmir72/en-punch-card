8ba883e3c7ba6f8d3f5b0f9be04f179407efb44c
Amirhossein Alisedaghat <amirhosseinalisedaghat@gmail.com>
2024-10-13 12:46:47 +0330

    start profile intigration

--- en-puch-card-app/src/app/components/Dashboard/Profile/profile-module/profile-module.module.ts
+++ en-puch-card-app/src/app/components/Dashboard/Profile/profile-module/profile-module.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { ProfileModuleRoutingModule } from './profile-module-routing.module';
 import { ProfileComponent } from '../profile/profile.component';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { MatIconModule } from '@angular/material/icon';
 
 
 @NgModule({
@@ -13,6 +14,7 @@ import { MatExpansionModule } from '@angular/material/expansion';
   imports: [
     CommonModule,
     MatExpansionModule,
+    MatIconModule,
     ProfileModuleRoutingModule,]
 })
 export class ProfileModuleModule { }
