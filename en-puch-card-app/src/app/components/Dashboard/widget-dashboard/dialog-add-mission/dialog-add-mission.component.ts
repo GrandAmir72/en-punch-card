490ade387b72e038c5df5bc915670b1b5de23f8d
Amirhossein Alisedaghat <amirhosseinalisedaghat@gmail.com>
2024-10-24 11:44:38 +0330

    mission component is done

--- en-puch-card-app/src/app/components/Dashboard/widget-dashboard/dialog-add-mission/dialog-add-mission.component.ts
+++ en-puch-card-app/src/app/components/Dashboard/widget-dashboard/dialog-add-mission/dialog-add-mission.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-dialog-add-mission',
@@ -6,5 +7,37 @@ import { Component } from '@angular/core';
   styleUrl: './dialog-add-mission.component.css'
 })
 export class DialogAddMissionComponent {
+  currentStep: number = 1;
+  progressBar:number = 0;
+  formHeader:string = 'Add Mission';
+  typeOfMission: 'h' | 'd' | undefined;
+  readonly dialog = inject(MatDialog);
 
+  nextStep(stepNumber: number) {
+    switch (stepNumber) {
+      case 1:
+        this.currentStep = 2;
+        this.progressBar = 50;
+        this.formHeader = 'Choose Type of mission';
+        break;
+      case 2:
+        this.progressBar = 100;
+        this.dialog.closeAll()
+        break;
+    }
+  }
+  preStep(stepNumber: number) {
+    switch (stepNumber) {
+      case 2:
+        this.currentStep = 1;
+        this.progressBar = 0;
+        this.formHeader = 'Add mission';
+        break;
+      case 1:
+        this.progressBar = 0;
+        // this.currentStep = 5;
+        
+        break;
+    }
+  }
 }
